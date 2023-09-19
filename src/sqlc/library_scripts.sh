6101bc344ea5c9ca6284aee2a9d4d4cd65bb2f25
Nick Z <2420177+nickzelei@users.noreply.github.com>
2023-09-19 00:29:04 +0000

    only use devcontainer base images in tests for now

--- src/sqlc/library_scripts.sh
+++ src/sqlc/library_scripts.sh
@@ -1,29 +1,7 @@
-#!/bin/bash -i
 
-# MIT License
-#
-# Copyright (c) 2022 devcontainers-contrib
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
 
 clean_download() {
-    # The purpose of this function is to download a file with minimal impact on contaier layer size
+    # The purpose of this function is to download a file with minimal impact on container layer size
     # this means if no valid downloader is found (curl or wget) then we install a downloader (currently wget) in a
     # temporary manner, and making sure to
     # 1. uninstall the downloader at the return of the function
@@ -37,7 +15,7 @@ clean_download() {
     tempdir=$(mktemp -d)
     downloader_installed=""
 
-    function _apt_get_install() {
+    _apt_get_install() {
         tempdir=$1
 
         # copy current state of apt list - in order to revert back later (minimize contianer layer size)
@@ -46,7 +24,7 @@ clean_download() {
         apt-get -y install --no-install-recommends wget ca-certificates
     }
 
-    function _apt_get_cleanup() {
+    _apt_get_cleanup() {
         tempdir=$1
 
         echo "removing wget"
@@ -57,7 +35,7 @@ clean_download() {
         rm -r /var/lib/apt/lists && mv $tempdir/lists /var/lib/apt/lists
     }
 
-    function _apk_install() {
+    _apk_install() {
         tempdir=$1
         # copy current state of apk cache - in order to revert back later (minimize contianer layer size)
         cp -p -R /var/cache/apk $tempdir
@@ -65,7 +43,7 @@ clean_download() {
         apk add --no-cache  wget
     }
 
-    function _apk_cleanup() {
+    _apk_cleanup() {
         tempdir=$1
 
         echo "removing wget"
@@ -121,45 +99,39 @@ ensure_nanolayer() {
     local variable_name=$1
 
     local required_version=$2
-    # normalize version
-    if ! [[ $required_version == v* ]]; then
-        required_version=v$required_version
-    fi
 
-    local nanolayer_location=""
+    local __nanolayer_location=""
 
     # If possible - try to use an already installed nanolayer
-    if [[ -z "${NANOLAYER_FORCE_CLI_INSTALLATION}" ]]; then
-        if [[ -z "${NANOLAYER_CLI_LOCATION}" ]]; then
+    if [ -z "${NANOLAYER_FORCE_CLI_INSTALLATION}" ]; then
+        if [ -z "${NANOLAYER_CLI_LOCATION}" ]; then
             if type nanolayer >/dev/null 2>&1; then
                 echo "Found a pre-existing nanolayer in PATH"
-                nanolayer_location=nanolayer
+                __nanolayer_location=nanolayer
             fi
         elif [ -f "${NANOLAYER_CLI_LOCATION}" ] && [ -x "${NANOLAYER_CLI_LOCATION}" ] ; then
-            nanolayer_location=${NANOLAYER_CLI_LOCATION}
-            echo "Found a pre-existing nanolayer which were given in env variable: $nanolayer_location"
+            __nanolayer_location=${NANOLAYER_CLI_LOCATION}
+            echo "Found a pre-existing nanolayer which were given in env variable: $__nanolayer_location"
         fi
 
         # make sure its of the required version
-        if ! [[ -z "${nanolayer_location}" ]]; then
+        if ! [ -z "${__nanolayer_location}" ]; then
             local current_version
-            current_version=$($nanolayer_location --version)
-            if ! [[ $current_version == v* ]]; then
-                current_version=v$current_version
-            fi
+            current_version=$($__nanolayer_location --version)
+
 
             if ! [ $current_version == $required_version ]; then
                 echo "skipping usage of pre-existing nanolayer. (required version $required_version does not match existing version $current_version)"
-                nanolayer_location=""
+                __nanolayer_location=""
             fi
         fi
 
     fi
 
     # If not previuse installation found, download it temporarly and delete at the end of the script
-    if [[ -z "${nanolayer_location}" ]]; then
+    if [ -z "${__nanolayer_location}" ]; then
 
-        if [ "$(uname -sm)" == "Linux x86_64" ] || [ "$(uname -sm)" == "Linux aarch64" ]; then
+        if [ "$(uname -sm)" = 'Linux x86_64' ] || [ "$(uname -sm)" = "Linux aarch64" ]; then
             tmp_dir=$(mktemp -d -t nanolayer-XXXXXXXXXX)
 
             clean_up () {
@@ -183,7 +155,7 @@ ensure_nanolayer() {
 
             tar xfzv $tmp_dir/$tar_filename -C "$tmp_dir"
             chmod a+x $tmp_dir/nanolayer
-            nanolayer_location=$tmp_dir/nanolayer
+            __nanolayer_location=$tmp_dir/nanolayer
 
 
         else
@@ -193,6 +165,7 @@ ensure_nanolayer() {
     fi
 
     # Expose outside the resolved location
-    declare -g ${variable_name}=$nanolayer_location
+    export ${variable_name}=$__nanolayer_location
 
 }
+
