a433e9d1d2e00cd414cad3f9526487e436c5d547
Nick Zelei <2420177+nickzelei@users.noreply.github.com>
2023-09-18 20:27:15 -0700

    Sqlc fix and general cleanup (#2)
    * remove lts check from version for sqlc
    
    * cleanup template features
    
    * update test feature matrix
    
    * remove sqlc readme to be autogen
    
    * fix if in install.sh
    
    * adds debian base to test matrix
    
    * only use devcontainer base images in tests for now
    
    * removing extra deps
    
    * no base image in test
    
    * re-enable base images
    
    * only use devcontainer images in test matrix

--- src/sqlc/install.sh
+++ src/sqlc/install.sh
@@ -1,14 +1,8 @@
 #!/bin/bash -i
 
-VERSION="${VERSION:-"latest"}"
-ORG="sqlc-dev"
-REPO="sqlc"
-
 set -e
 
-binary_names="sqlc"
-
-source ./library_scripts.sh
+. ./library_scripts.sh
 
 # nanolayer is a cli utility which keeps container layers as small as possible
 # source code: https://github.com/devcontainers-contrib/nanolayer
@@ -17,22 +11,13 @@ source ./library_scripts.sh
 # of the script
 ensure_nanolayer nanolayer_location "v0.5.3"
 
-# fetch latest version if needed
-if [ "${VERSION}" = "latest" ] || [ "${VERSION}" = "lts" ]; then
-    tag=$(curl -s https://api.github.com/repos/$ORG/$REPO/releases/latest | jq -r .tag_name)
-    export VERSION="${tag:1}"
-fi
-
 $nanolayer_location \
     install \
     devcontainer-feature \
     "ghcr.io/devcontainers-contrib/features/gh-release:1.0.21" \
         --option repo='sqlc-dev/sqlc' \
-        --option binaryNames="$binary_names" \
+        --option binaryNames='sqlc' \
         --option version="$VERSION" \
         --option assetRegex='.*\.tar\.gz' \
-        --option libName='sqlc'
-
-
 
 echo 'Done!'
